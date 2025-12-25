9182c5ffc4df00d3da5de62ac8a3310474d2adea
Kevin SEMITEJA <kevinsemiteja5@gmai.com>
2025-12-25 20:19:03 +0100

    Config: Point frontend to production API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000', // L'adresse de votre Backend
+  const API_URL = 'https://horizon-api.onrender.com';
   headers: {
     'Content-Type': 'application/json',
   },
