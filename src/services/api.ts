ca03cae6dc2f06f025f75607d413fa9d070f8fb4
Kevin SEMITEJA <kevinsemiteja5@gmai.com>
2025-12-25 20:46:07 +0100

    Fix: Api syntax and build script for React

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+// 1. DÉCLARER LA CONSTANTE ICI (En dehors de l'objet)
+const API_URL = 'https://horizon-api.onrender.com';
+
+// 2. CRÉER L'INSTANCE
 export const api = axios.create({
-  const API_URL = 'https://horizon-api.onrender.com';
+  baseURL: API_URL, // Utilisation de la variable
   headers: {
     'Content-Type': 'application/json',
   },
